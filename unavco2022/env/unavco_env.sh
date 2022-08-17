596db4f78184fc7c228f64908d9bcced90798a69
Rui Kawahara <rkawahara@alaska.edu>
2022-08-17 14:41:13 -0800

    works without --user root

--- unavco2022/env/unavco_env.sh
+++ unavco2022/env/unavco_env.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 set -e
 
-# Assume gitpuller already installed
-
-PATH=$HOME/.local/bin:$PATH
-
+# /opt/conda/envs/unavco
 ############### Copy to .local/envs ###############
-LOCAL="$HOME"/.local
+CONDA=/opt/conda
 NAME=unavco
-SITE_PACKAGES="$LOCAL/envs/$NAME/lib/python3.8/site-packages"
+SITE_PACKAGES="$CONDA/envs/$NAME/lib/python3.8/site-packages"
 ##############################################################
+
+PATH="$CONDA/envs/$NAME/bin:$PATH"
 
 #conda run -n "$NAME" kernda --display-name $NAME $LOCAL/envs/$NAME/share/jupyter/kernels/python3/kernel.json -o
 
@@ -20,61 +19,37 @@ path="$PATH"
 
 # start building local path and pythonpath variables
 pythonpath=$SITE_PACKAGES/isce:"$pythonpath"
-path="$SITE_PACKAGES"/isce/applications:"$LOCAL"/envs/"$NAME"/bin:$path
+path="$SITE_PACKAGES"/isce/applications:$path
 
 # set ISCE_HOME
 conda env config vars set -n $NAME ISCE_HOME="$SITE_PACKAGES"/isce
 
-######## Install MintPy ########
-
-MINTPY_HOME="$LOCAL"/MintPy
-PYAPS_HOME="$LOCAL"/PyAPS
-
-# set MintPy env variables
-conda env config vars set -n $NAME MINTPY_HOME="$MINTPY_HOME"
-conda env config vars set -n $NAME PYAPS_HOME="$PYAPS_HOME"
-
-#update local path and pythonpath variables
-path="$MINTPY_HOME"/mintpy:"$path"
-pythonpath="$MINTPY_HOME":"$PYAPS_HOME":"$pythonpath"
-
-# clone MintPy
-if [ ! -d "$MINTPY_HOME" ]
-then
-    git clone -b v1.3.1 --depth=1 --single-branch https://github.com/insarlab/MintPy.git "$MINTPY_HOME"
-fi
-
-# clone pyaps
-if [ ! -d "$PYAPS_HOME" ]
-then
-    git clone -b main --depth=1 --single-branch https://github.com/yunjunz/PyAPS.git "$PYAPS_HOME"
-fi
-
 ######## Install ARIA-Tools ########
 
 # clone the ARIA-Tools repo and build ARIA-Tools
-aria="$LOCAL/ARIA-tools"
-if [ ! -d "$aria" ]
+ARIA=/aria
+ARIATOOLS="$ARIA/ARIA-tools"
+if [ ! -d "$ARIATOOLS" ]
 then
-    git clone -b release-v1.1.2 https://github.com/aria-tools/ARIA-tools.git "$aria"
+    git clone https://github.com/aria-tools/ARIA-tools.git "$ARIATOOLS"
     wd=$(pwd)
-    cd "$aria"
-    conda run -n $NAME python "$aria"/setup.py build
-    conda run -n $NAME python "$aria"/setup.py install
+    cd "$ARIATOOLS"
+    conda run -n $NAME python "$ARIATOOLS"/setup.py build
+    conda run -n $NAME python "$ARIATOOLS"/setup.py install
     cd "$wd"
 fi
 
-path="$LOCAL/ARIA-tools/tools/bin:$LOCAL/ARIA-tools/tools/ARIAtools:"$path
-pythonpath="$LOCAL/ARIA-tools/tools:$LOCAL/ARIA-tools/tools/ARIAtools:"$pythonpath
+path="$ARIATOOLS/ARIA-tools/tools/bin:$ARIATOOLS/ARIA-tools/tools/ARIAtools:"$path
+pythonpath="$ARIATOOLS/ARIA-tools/tools:$ARIATOOLS/ARIA-tools/tools/ARIAtools:"$pythonpath
 conda env config vars set -n $NAME GDAL_HTTP_COOKIEFILE=/tmp/cookies.txt
 conda env config vars set -n $NAME GDAL_HTTP_COOKIEJAR=/tmp/cookies.txt
 conda env config vars set -n $NAME VSI_CACHE=YES
 
 # clone the ARIA-tools-docs repo
-aria_docs="/home/jovyan/ARIA-tools-docs"
-if [ ! -d $aria_docs ]
+ARIADOCS="$ARIA/ARIA-tools-docs"
+if [ ! -d $ARIADOCS ]
 then
-    git clone -b master --depth=1 --single-branch https://github.com/aria-tools/ARIA-tools-docs.git $aria_docs
+    git clone -b master --depth=1 --single-branch https://github.com/aria-tools/ARIA-tools-docs.git $ARIADOCS
 fi
 
 #######################
