9d82e06db9bb0b51fac100163e29508dc403306d
Rui Kawahara <rkawahara@alaska.edu>
2022-08-24 10:50:53 -0800

    removed topbar for localized version

--- unavco2022/etc/startup.sh
+++ unavco2022/etc/startup.sh
@@ -2,25 +2,6 @@
 
 set -ex
 
-########################## work but no sync ##########################
-
-# if [ ! -d $HOME/Geo-SInC ];
-# then
-#   mv -v /etc/unavco/Geo-SInC $HOME
-# else
-#   rm -rf /etc/unavco/Geo-SInC
-# fi
-
-# if [ ! -d $HOME/conda_environments ];
-# then
-#   mv -v /etc/unavco/conda_environments $HOME
-# else
-#   rm -rf /etc/unavco/conda_environments
-# fi
-
-########################## work but no sync ##########################
-
-
 # python -m pip install --user \
 #     ipywidgets \
 #     mpldatacursor
@@ -142,70 +123,3 @@ c.InteractiveShellApp.extensions = ['df_jupyter_magic']
 c.NotebookApp.kernel_manager_class = ['notebook.services.kernels.kernelmanager.AsyncMappingKernelManager']
 EOT
 fi
-
-
-#############################################################################
-# #!/bin/bash
-# set -ve
-
-# # python /etc/jupyter-hooks/resource_checks/check_storage.py $1
-
-# # Add Path to local pip execs.
-# export PATH=$HOME/.local/bin:$PATH
-
-# python -m pip install --user nbgitpuller
-
-# # copy over our version of pull.py
-# # REMINDER: REMOVE IF CHANGES ARE MERGED TO NBGITPULLER
-# cp /etc/jupyter-hooks/scripts/pull.py /home/jovyan/.local/lib/python3.9/site-packages/nbgitpuller/pull.py
-
-# # Disable the extension manager in Jupyterlab since server extensions are uninstallable
-# # by users and non-server extension installs do not persist over server restarts
-# jupyter labextension disable @jupyterlab/extensionmanager-extension
-
-# mkdir -p $HOME/.ipython/profile_default/startup/
-# cp /etc/jupyter-hooks/custom_magics/00-df.py $HOME/.ipython/profile_default/startup/00-df.py
-
-# gitpuller https://github.com/parosen/Geo-SInC.git main $HOME/Geo-SInC
-
-# gitpuller https://github.com/ASFOpenSARlab/opensarlab-envs.git main $HOME/conda_environments
-
-# # Update page and tree
-# mv /opt/conda/lib/python3.9/site-packages/notebook/templates/tree.html /opt/conda/lib/python3.9/site-packages/notebook/templates/original_tree.html
-# cp /etc/jupyter-hooks/templates/tree.html /opt/conda/lib/python3.9/site-packages/notebook/templates/tree.html
-
-# mv /opt/conda/lib/python3.9/site-packages/notebook/templates/page.html /opt/conda/lib/python3.9/site-packages/notebook/templates/original_page.html
-# cp /etc/jupyter-hooks/templates/page.html /opt/conda/lib/python3.9/site-packages/notebook/templates/page.html
-
-# CONDARC=$HOME/.condarc
-# if ! test -f "$CONDARC"; then
-# cat <<EOT >> $CONDARC
-# channels:
-#   - conda-forge
-#   - defaults
-
-# channel_priority: strict
-
-# envs_dirs:
-#   - /home/jovyan/.local/envs
-#   - /opt/conda/envs
-# EOT
-# fi
-
-# # Add a CondaKernelSpecManager section to jupyter_notebook_config.json to display nicely formatted kernel names
-# JN_CONFIG=$HOME/.jupyter/jupyter_notebook_config.json
-# if ! test -f "$JN_CONFIG"; then
-# echo '{}' > "$JN_CONFIG"
-# fi
-
-# if ! grep -q "\"CondaKernelSpecManager\":" "$JN_CONFIG"; then
-# jq '. += {"CondaKernelSpecManager": {"name_format": "{display_name}"}}' "$JN_CONFIG" >> temp;
-# mv temp "$JN_CONFIG";
-# fi
-
-# conda init
-
-# BASH_PROFILE=$HOME/.bash_profile
-# if ! test -f "$BASH_PROFILE"; then
-# cat <<EOT>> $BASH_PROFILE
-# if [ -s ~/.bashrc ]; then
