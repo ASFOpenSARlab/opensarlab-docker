079ebb260ee2cc83f735597dab121cd7787e06a2
Eric Lundell <emlundell@alaska.edu>
2022-08-25 12:38:17 -0800

    Add note on amended license

--- iris2022/start_container.sh
+++ iris2022/start_container.sh
@@ -2,4 +2,4 @@
 
 set -ex
 
-docker run -it --init --rm -p 8888:8888 -v $(pwd)/iris_home:/home/jovyan iris2022:latest
+docker run -it --init --rm -p 8888:8888 -v $(pwd)/virtual_home:/home/jovyan iris2022:latest
